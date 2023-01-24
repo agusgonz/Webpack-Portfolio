78d3bf104a580e09dfe2083106bcab18bbd7b3be
agusgonz <agustingendeiza@gmail.com>
2023-01-24 11:07:55 -0300

    COMMIT ALL THE FILES!

---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+
+/** @type {import('webpack').Configuration} */
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    assetModuleFilename: 'assets/[path][name][ext]',
+  },
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+      '@templates': path.resolve(__dirname, 'src/templates/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
+      '@fonts': path.resolve(__dirname, 'src/assets/fonts/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        //Asset Modules(viene integrado a webpack)
+        test: /\.(png|svg|gif|jpg|jpeg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[contenthash][ext][query]'
+        },
+      },
+      {
+        test: /\.(woff|woff2)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[contenthash][ext][query]'
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'JS Portafolio 2',
+      filename: 'index.html',
+      template: './public/index.html',
+      inject: 'true',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/styles/[name].[contenthash].css'
+    }),
+    // new CopyPlugin({
+    //   patterns: [
+    //     {from: path.resolve(__dirname, 'src', 'assets/images'), to: 'assets/images' }
+    //   ]
+    // })
+    new Dotenv(),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()]
+  }
+}
